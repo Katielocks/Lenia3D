990e3ec748effe823dd9bbfc8ba4895767f03171
Katielocks <115801010+Katielocks@users.noreply.github.com>
2023-04-24 16:01:00 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,18 +1,7 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  5 00:39:23 2023
-
-@author: ktwhi
-"""
 import torch
 import numpy as np
 
-import plotly.graph_objs as go
-import plotly.graph_objects as go
-import plotly.io as pio
-pio.renderers.default = 'browser'
 
-import mesh
 
 def generate_random_state( cluster_num, cluster_size, grid_size,seed=None):
     """
@@ -25,7 +14,7 @@ def generate_random_state( cluster_num, cluster_size, grid_size,seed=None):
    - grid_size (int): Size of the 3D grid to generate.
 
    Returns:
-   - grid (torch.Tensor): 3D tensor representing the generated grid.
+   - grid (numpy.array): 3D tensor representing the generated grid.
    """
     if seed == None:
         seed = torch.randint(0,10**8,(1,)).item()
@@ -42,7 +31,7 @@ def generate_random_state( cluster_num, cluster_size, grid_size,seed=None):
         new_indices = torch.remainder(tensor.indices().t()+cluster,grid_size)
         cluster_coo  = torch.sparse_coo_tensor(new_indices.t() , tensor.values(),tuple([grid_size]*3))
         grid += cluster_coo
-    grid = grid.to_dense()
+    grid = grid.to_dense().numpy()
     
     return grid
 
@@ -56,14 +45,15 @@ def generate_kernel(radius, grid_size):
     - grid_size (int): Size of the 3D grid.
 
     Returns:
-    - fK (torch.Tensor): 3D tensor representing the generated kernel.
+    - fK (numpy.array): 3D tensor representing the generated kernel.
     """
     mid = grid_size//2  
     bell = lambda x, m, s: np.exp(-((x-m)/s)**2 / 2)
-    D = np.linalg.norm(np.ogrid[-mid:mid, -mid:mid,-mid:mid]) / radius
+    x, y, z = np.meshgrid(np.arange(-mid, mid), np.arange(-mid, mid), np.arange(-mid, mid))
+    D = np.sqrt(x**2 + y**2 + z**2) / radius
     K = (D<1) * bell(D, 0.5, 0.15)
+    K = K / np.sum(K)
     fK = np.fft.fftn(np.fft.fftshift(K / np.sum(K)))
-    fK = torch.tensor(fK)
     return fK
 
 
@@ -72,17 +62,17 @@ def golupdate(grid,kernel,frames_num,m,s):
     Updates the input grid using the Game of Life rules.
     
     Parameters:
-    - grid (torch.Tensor): 3D tensor representing the input grid.
-    - kernel (torch.Tensor): 3D tensor representing the kernel to use for the update.
+    - grid (numpy.array): 3D tensor representing the input grid.
+    - kernel (numpy.array): 3D tensor representing the kernel to use for the update.
     - frames_num (int): Number of frames to update the grid.
     - m (float): Mean value for the growth function.
     - s (float): Standard deviation value for the growth function.
     
     Returns:
-    - grid (torch.Tensor): 3D tensor representing the updated grid.
+    - grid (numpy.array): 3D tensor representing the updated grid.
     """
-    U = torch.real(torch.fft.ifftn(kernel * torch.fft.fftn(grid)))
-    grid = torch.clamp(grid + 1/frames_num * growth(U,m,s), 0, 1)
+    U = np.real(np.fft.ifftn(kernel * np.fft.fftn(grid)))
+    grid = np.clip(grid + 1/frames_num * growth(U,m,s), 0, 1)
     return grid
 
 def growth(U,m,s): 
@@ -90,13 +80,14 @@ def growth(U,m,s):
     Computes the growth function for updating the grid using the Game of Life rules.
 
     Parameters:
-    - U (torch.Tensor): 3D tensor representing the input for the growth function.
+    - U (numpy.array): 3D tensor representing the input for the growth function.
     - m (float): Mean value for the growth function.
     - s (float): Standard deviation value for the growth function.
 
     Returns:
-    - result (torch.Tensor): 3D tensor representing the computed growth function.
+    - result (numpy.array): 3D tensor representing the computed growth function.
     """
     bell = lambda x, m, s: np.exp(-((x-m)/s)**2 / 2)
     return bell(U, m, s)*2-1
+
 
