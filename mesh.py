1a7ababb754d84ea596ab43681c971f180842812
Katielocks <115801010+Katielocks@users.noreply.github.com>
2023-04-24 15:57:57 +0100

    Update mesh.py

--- mesh.py
+++ mesh.py
@@ -7,65 +7,33 @@ Created on Wed Apr  5 13:17:57 2023
 import torch
 import numpy as np
 def generate_voxel(grid, offset, offset_faces):
-    """
-    mesh_gen : a function which takes in indices of a 3d tensor and generates a simplifed voxel mesh
-    
-    grid : 3d pytorch tensor
-    offset : relative vertices
-    offset_faces : faces of relative vertices
-    
-    """
-    #get nonzero indices and generate a face index tensor
-    indices = grid.indices().t()   
-    faces = torch.arange(len(indices)).reshape(len(indices), 1) 
-    
-    #create a tensor representing face color 
-    face_colors  = grid.values().repeat_interleave(6)
-    
-    #Apply offset faces to faces
-    faces = 8 * faces.view(faces.shape[0], 1, faces.shape[1]) + offset_faces 
-    faces = faces.view(-1, faces.shape[-1]) 
-
+    # Get the non-zero indices of the grid and generate a face index array
+    indices = grid.nonzero()
+    # Create a array representing face color 
+    face_colors  = np.repeat(grid[indices],6)
+    #generate indices and faces arrays
+    indices = np.transpose(np.stack(indices)) 
+    faces = np.arange(len(indices)).reshape(len(indices), 1) 
+    # Apply offset faces to faces
+    faces = 8 * faces.reshape(faces.shape[0], 1, faces.shape[1]) + offset_faces 
+    faces = faces.reshape(-1, faces.shape[-1]) 
     # Apply offset to each vertex
-    vertices = indices.view(indices.shape[0], 1, indices.shape[1]) + offset + 1 
+    vertices = indices.reshape(indices.shape[0], 1, indices.shape[1]) + offset
     vertices = vertices.reshape(len(vertices) * 8, 3)
-
-    # Generate a index map for sorting indices
-    vertices_flipped = torch.flip(vertices, [1]).numpy() 
-    
-    verts_map = torch.tensor(np.lexsort(vertices_flipped.T))
-    vertices = vertices[verts_map]
-    
-    #realign faces
-    inv_vert_map = torch.empty_like(verts_map)
-    inv_vert_map[verts_map] = torch.arange(len(verts_map))
-    faces = inv_vert_map[faces]
-
-    # represent vertices as a sparse tensor, and remove duplicate vertices
-    vertices_coo = torch.sparse_coo_tensor(2 * vertices.t(), torch.ones(len(vertices)), tuple((2 * torch.tensor(grid.shape) + 1).tolist()))
-    vertices_coo = vertices_coo.coalesce()
-    vertices = vertices_coo.indices().t() / 2
-    values = vertices_coo.values()
-
-    # generate map from vertices which have duplicates to non duplicate vertices
-    vertex_map = torch.arange(len(values)).repeat_interleave(values.long())
-    faces = vertex_map[faces]
-    
-    #sorts faces to match faces_coo output
-    sorted_faces = torch.flip(torch.sort(faces, dim=1)[0], [1]).numpy()
-    index_map = torch.tensor(np.lexsort(sorted_faces.T))
-    faces = faces[index_map]
-    #create a count of faces
-    faces_coo = torch.sparse_coo_tensor(torch.sort(faces, dim=1)[0].t()[:3], torch.ones(len(faces)), tuple([len(vertices)] * 3))
-    faces_coo = faces_coo.coalesce()
-    values = faces_coo.values()
-
-    # Filter out faces which have atleast one duplicate
-    mask = (values < 2).repeat_interleave(values.long())
+    # Remove duplicate vertices
+    vertices, index,counts = np.unique(vertices, axis=0, return_inverse=True,return_counts=True)
+    faces = index[faces]
+    # generate mask for unique faces
+    center = np.mean(vertices[faces],axis=1)
+    center, index, count = np.unique(center, axis=0, return_index=True,return_counts=True)
+    center = center[count<2]
+    index = index[count<2]
+    mask = np.zeros(len(faces), dtype=bool)
+    mask[index] = True
+    #Apply mask
     faces = faces[mask]
-    face_colors = face_colors[mask]
-    # Return the vertices, square indices, and color values
-    return vertices, faces, face_colors
+    face_values = face_colors[mask]
+    return vertices, faces, face_values
 
 def generate_triangles(shapes):
     """
