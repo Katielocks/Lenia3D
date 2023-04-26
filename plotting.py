60e4c29228ef54c6ad72e32e54d5d9930885313e
Katielocks <115801010+Katielocks@users.noreply.github.com>
2023-04-26 15:07:54 +0100

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -2,7 +2,6 @@
 
 
 import plotly.graph_objs as go
-import plotly.graph_objects as go
 import plotly.io as pio
 pio.renderers.default = 'browser'
 
@@ -22,27 +21,6 @@ def plot_mesh(time,frames,radius,m,s,grid_size,cluster_num = 40,cluster_size = (
     grid_size: the size of the grid on which the simulation is run
     cluster_num: the number of clusters to generate in the initial state of the system (default value is 40)
     cluster_size: a tuple representing the range of sizes for the clusters generated in the initial state of the system (default value is (2,8))"""
-    # Define the 3D coordinates of the vertices of a cube
-    offset = np.array([
-        [-0.5, -0.5, -0.5], # vertex 0
-        [ 0.5, -0.5, -0.5], # vertex 1
-        [ 0.5,  0.5, -0.5], # vertex 2
-        [-0.5,  0.5, -0.5], # vertex 3
-        [-0.5, -0.5,  0.5], # vertex 4
-        [ 0.5, -0.5,  0.5], # vertex 5
-        [ 0.5,  0.5,  0.5], # vertex 6
-        [-0.5,  0.5,  0.5], # vertex 7
-    ])
-
-    # Define the faces of the cube as offsets into the vertex tensor
-    offset_faces = np.array([
-        [0, 1, 2, 3], # face 0
-        [1, 5, 6, 2], # face 1
-        [4, 0, 3, 7], # face 2
-        [5, 4, 7, 6], # face 3
-        [3, 2, 6, 7], # face 4
-        [4, 5, 1, 0], # face 5
-    ])
     layout = go.Layout(
         scene=dict(
         xaxis=dict(range=[0, grid_size] ,showgrid=False,color='black'),
@@ -67,9 +45,9 @@ def plot_mesh(time,frames,radius,m,s,grid_size,cluster_num = 40,cluster_size = (
     )]
     )  
     # Generate a state grid using the state_gen function
-    grid = f.generate_random_state(cluster_num,cluster_size,grid_size,seed=None)
+    grid = f.generate_random_state(cluster_num,cluster_size,grid_size,seed=seed)
     # Convert the state grid to a sparse tensor and coalesce it to eliminate duplicates
-    frame = generate_frame(grid, offset,offset_faces)
+    frame = generate_frame(grid)
     
     # Create a list of frames to be used for animation
     frames_list = [frame]
@@ -79,9 +57,10 @@ def plot_mesh(time,frames,radius,m,s,grid_size,cluster_num = 40,cluster_size = (
     # Run the Game of Life update loop
     for i in range(time*frames):
         grid = f.golupdate(grid,kernel,frames,m,s)
-        frames_list.append(generate_frame(grid, offset,offset_faces))
-        
-        
+        if np.any(grid):
+            frames_list.append(generate_frame(grid))
+        else:
+            break
     # Create figure object
     fig = go.Figure(data=[], layout=layout, frames=frames_list)
     fig.add_trace(frames_list[0].data[0])
@@ -90,9 +69,9 @@ def plot_mesh(time,frames,radius,m,s,grid_size,cluster_num = 40,cluster_size = (
     
     
     
-def generate_frame(grid, offset,offset_faces,colorscale='thermal'):
+def generate_frame(grid):
     
-    vertices,squares,sq_values = mesh.generate_voxel(grid,offset,offset_faces)
+    vertices,squares,sq_values = mesh.generate_voxel(grid)
     triangles =  mesh.generate_triangles(squares)
     sq_values = sq_values.repeat(2)
     
@@ -105,9 +84,8 @@ def generate_frame(grid, offset,offset_faces,colorscale='thermal'):
     j=triangles[:, 1],
     k=triangles[:, 2],
     flatshading=True,
-    colorscale=colorscale,
+    colorscale='thermal',
     intensity=sq_values
     )
     
     return go.Frame(data=[trace])
-    
